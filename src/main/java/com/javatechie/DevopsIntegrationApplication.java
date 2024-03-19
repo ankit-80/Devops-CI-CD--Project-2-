366144a39b0aee8cf55f250df367cb0403e64563
ankit-80 <157349284+ankit-80@users.noreply.github.com>
2024-03-19 09:58:44 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,8 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to Young Minds";
+		return "welcome  ";
+		return "this devops project run successfully  ";
 	}
 
 	public static void main(String[] args) {
