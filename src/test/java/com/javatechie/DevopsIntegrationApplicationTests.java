53e418761dcc60ad4a87776c1add9064612ae271
Ankit <157349284+ankit-80@users.noreply.github.com>
2026-01-08 19:41:58 +0530

    Update DevopsIntegrationApplicationTests.java

--- src/test/java/com/javatechie/DevopsIntegrationApplicationTests.java
+++ src/test/java/com/javatechie/DevopsIntegrationApplicationTests.java
@@ -1,14 +1,13 @@
 package com.javatechie;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-public class DevopsIntegrationApplication {
+@SpringBootTest
+class DevopsIntegrationApplicationTests {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DevopsIntegrationApplication.class, args);
-        System.out.println("DevOps Integration Application Started Successfully");
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
